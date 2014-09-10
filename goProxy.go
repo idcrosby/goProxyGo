e98bb98e05ef40a5efe9bca1a951084bc611b94b
idcrosby <ian.daniel.crosby@gmail.com>
2014-09-10 17:15:53 +0200

    Added working resource expansion, error return from goget function.

--- goProxy.go
+++ goProxy.go
@@ -2,26 +2,28 @@ package goProxy
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"github.com/idcrosby/web-tools"
 	"net/http"
 	"strings"
 )
 
-func GoGet(url string) []byte {
+func GoGet(url string) ([]byte, error) {
 	res, err := http.Get(url)
-	check(err)
+	// check(err)
 
 	body, err := ioutil.ReadAll(res.Body)
-	check(err)
-	return body
+	// check(err)
+	return body, err
 }
 
 func GoGetAndFilter(url string, filter []string, pretty bool) []byte {
 
-	body := GoGet(url)
-	buf, err := myTools.JsonPositiveFilter(body, filter, pretty)
+	body, err := GoGet(url)
 	check(err)
+	buf, err1 := myTools.JsonPositiveFilter(body, filter, pretty)
+	check(err1)
 
 	return buf
 }
@@ -42,12 +44,12 @@ func HateoasExpand(body []byte, expand []string) (buf []byte, err error) {
 			//Check if last element
 			if (index >= (len(subEls) -1)) {
 				//delete(node, sub)
-				expandThis(node, sub)
+				expandField(node, sub)
 			} else {
 				node = node[sub].(map[string]interface{})
 			}
 		}
-		expandThis(m, element)
+		// expandField(m, element)
 		//delete(m, element)
 	}
 	buf, err = json.MarshalIndent(&m, "", "   ")
@@ -55,23 +57,28 @@ func HateoasExpand(body []byte, expand []string) (buf []byte, err error) {
 	return
 }
 
-func expandThis(baseMap map[string]interface{}, el string) {
+func expandField(baseMap map[string]interface{}, el string) {
+	fmt.Printf("expand field %s \n", el)
 	// TODO handle potential casting error
-	var url string
+	// var url string
 	if subMap := baseMap[el].(map[string]interface{}); subMap != nil {
-		links := subMap["links"]
-		switch links.(type) {
-			case string:
-				url = links.(string)
-			case map[string]interface{}:
-				url = "object" //links.(map[string]interface{})["href"]
-			case []interface{}:
-				// subLink := links.([]interface{})[0]
-				url = "array" // subLink.(map[string]interface{})["href"]
-			default:
-				// TODO throw error
-		}
-		baseMap[el] = GoGet(url)
+		// links := subMap["links"]
+		// switch links.(type) {
+		// 	case string:
+		// 		url = links.(string)
+		// 	case map[string]interface{}:
+		// 		url = "object" //links.(map[string]interface{})["href"]
+		// 	case []interface{}:
+		// 		// subLink := links.([]interface{})[0]
+		// 		url = "array" // subLink.(map[string]interface{})["href"]
+		// 	default:
+		// 		// TODO throw error
+		// }
+
+		// for initial implementation, only handle basic `href` field
+		href := subMap["href"]
+		// TODO this is base 64 encoding the response for some reason??
+		baseMap[el], _ = GoGet(href.(string))
 	}
 }
 
--- goProxy_test.go
+++ goProxy_test.go
@@ -7,7 +7,7 @@ import(
 func TestGet(t *testing.T) {
 	url := "http://example.com"
 	data := "..."
-	if x := GoGet(url); x == nil {
+	if x, err := GoGet(url); x == nil || err != nil {
 		t.Errorf("Get(" + url + ") = " + string(x) + ", want " + data)
 	}	
 }
@@ -19,4 +19,22 @@ func TestFilterGet(t *testing.T) {
 	if x := GoGetAndFilter(url, filter, false); string(x) != data  {
 		t.Errorf("GoGetAndFilter(" + url + ") = " + string(x) + ", want " + data)
 	}	
+}
+
+func TestHateoasExpand(t *testing.T) {
+	input := []byte("{\"one\":true,\"two\":{\"href\":\"http://api.icndb.com/jokes/15\"}}")
+	// url := "http://api.icndb.com/jokes/15"
+	expand := []string{"two"}
+	data := "{\"one\":true,\"two\":{\"value\":{\"joke\":\"When Chuck Norris goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket.\"}}}"
+	if x, err := HateoasExpand(input, expand); string(x) != data  || err != nil {
+		t.Errorf("HateoasExpand(" + string(input) + ", " + arrayToString(expand) + ") = " + string(x) + ", want " + data)
+	}	
+}
+
+
+// Util Methods
+
+func arrayToString(input []string) (output string) {
+	for _, value := range input { output += string(value) }
+	return
 }
